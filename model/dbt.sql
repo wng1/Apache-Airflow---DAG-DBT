e8b71b8fe00edbcb91ecafa6459a26c8b1b934f2
W N <wng1@users.noreply.github.com>
2025-02-12 22:02:26 +0000

    Create dbt.sql

---
+++ model/dbt.sql
@@ -0,0 +1,23 @@
+'Common Table Expresssion - CTE (Temporary Alias)'
+
+with audioplayers as (
+  select * from products
+),
+'Summarise Taxi Rides'
+  
+final as (
+  SELECT
+    vendorID,
+    CAST(SoldDateTimme AS DATE) AS purchase_date,
+    COUNT(*) AS purchase_count
+  FROM
+    [company-data-warehouse].[dbo].[products]
+  GROUP BY
+    vendorID,
+    CAST(SoldDateTime AS DATE)
+  ORDER BY
+    vendorID,
+    purchase_date;
+)
+select * from final
+
