6273cf0f5db3ea613b2842010eef74dde9b7b049
W N <wng1@users.noreply.github.com>
2025-02-19 17:25:49 +0000

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -4,3 +4,12 @@ SELECT * FROM Products;
 SELECT * FROM ProductPrice;
 SELECT * FROM ProductType;
 
+'Passing variablesi n dbt commands
+
+dbt run --vars '{"volume_path": "__________________", "date": "19/02/2025"}'
+dbt run '{"volume_path":"{{job.parameters.volume_path}}"}'
+dbt run {{job.parameters.select_clause}}
+dbt run {{job.parameters.dbt_refresh}}
+dbt run --select "{{job.parameters.table_name}}"
+dbt run '{"volume_path":"{{job.parameters.volume_path}}"}' {{job.parameters.dbt_refresh}}
+
